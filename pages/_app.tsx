896ed59186f9320691169ab1e58b70f053449996
Javier Castro <jcastromarchant@gmail.com>
2022-04-11 13:22:52 -0400

    chore: husky pre-commit, linter, and prettier config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+/* eslint-disable react/jsx-props-no-spreading */
+import "styles/global.css"
+import { NextPage } from "next"
+import { ReactElement, ReactNode } from "react"
+import type { AppProps } from "next/app"
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export type NextPageWithLayout = NextPage & {
+    getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+    Component: NextPageWithLayout
+}
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+    const getLayout = Component.getLayout ?? ((page) => page)
+
+    return getLayout(<Component {...pageProps} />)
 }
 
 export default MyApp
